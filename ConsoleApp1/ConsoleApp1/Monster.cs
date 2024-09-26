8806aaed37b946dba2abf98b62b19f54e3020317
gwanyoungkim719 <0000kgy@naver.com>
2024-09-26 19:32:30 +0900

    Create Monster.cs

---
+++ ConsoleApp1/ConsoleApp1/Monster.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Monster
+    {
+
+        int level;
+        string name;
+        int hp;
+        int atk;
+
+        public void MonsterInfo()
+        {
+
+            Console.WriteLine($"Lv.{level} {name}  HP {hp}");
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
