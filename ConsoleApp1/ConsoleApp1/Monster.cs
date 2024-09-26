4eb7f4bdbb6a615a935704efaad51dbbae547565
gwanyoungkim719 <0000kgy@naver.com>
2024-09-26 21:09:56 +0900

    [UPDATE]Monster

--- ConsoleApp1/ConsoleApp1/Monster.cs
+++ ConsoleApp1/ConsoleApp1/Monster.cs
@@ -1,22 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     public class Monster
     {
+        
+        public int level;
+        public string name;
+        public int hp;
+        public int def;
+        public int atk;
 
-        int level;
-        string name;
-        int hp;
-        int atk;
+        
+        //몬스터 정보
+        //몬스터 스폰을 랜덤하게
+        
+
 
         public void MonsterInfo()
         {
-
+            
             Console.WriteLine($"Lv.{level} {name}  HP {hp}");
 
         }
 
+
+        public int Monster(int monster)
+        {
+           
+            
+
+        }
+
+        
+        
 
 
 
